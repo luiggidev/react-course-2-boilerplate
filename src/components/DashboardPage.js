dc1fcc504cda339c300a6c406c73051e2fbbd5f7
Luiggi Annes Bellincanta <65421097+luiggidev@users.noreply.github.com>
2022-02-25 14:49:39 -0300

    add content-container to dashboard

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const DashboardPage = () => (
-  <div>
-    Dashboard page content
+  <div className='content-container'>
+    <h1>Dashboard page content</h1>
   </div>
 );
 
